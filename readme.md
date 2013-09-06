8a3efcfde9a08c063c9947678763a86f3676ca0b
Chad Zawistowski <chad.zawistowski@gmail.com>
2013-09-06 17:10:36 -0400

    Add code from Phasor

--- readme.md
+++ readme.md
@@ -6,4 +6,6 @@ A template for hooking into the Halo PC console to provide additional commands a
 
 The entirety of my work on this project is released under the 2-clause BSD license.
 
-'src\Launcher\HCommonEnsureCleanup.h' was provided courtesy of [GameDeception.net](http://www.gamedeception.net/forums/184-Windows-Internals) and its license is unclear.
+The majority of 'src/Bootstrapper/Bootstrapper.cpp' is sourced from the [Phasor](https://github.com/urbanyoung/Phasor) project, the license of which is unclear. (Working to resolve this)
+
+'src/Launcher/HCommonEnsureCleanup.h' was provided courtesy of [GameDeception.net](http://www.gamedeception.net/forums/184-Windows-Internals) and its license is unclear.
--- src/Bootstrapper/Bootstrapper.cpp
+++ src/Bootstrapper/Bootstrapper.cpp
@@ -1,6 +1,209 @@
 #include "stdafx.h"
 
+//
+//-------------------------------------------------------------------------------------------------
+// Memory Functions
+//
+
+// Writes to process memory
+BOOL WriteBytes(DWORD dwAddress, LPVOID lpBuffer, DWORD dwCount)
+{
+	BOOL bResult = TRUE;
+
+	HANDLE hProcess = GetCurrentProcess();
+	LPVOID lpAddress = UlongToPtr(dwAddress);
+
+	DWORD dwOldProtect;
+	DWORD dwNewProtect = PAGE_EXECUTE_READWRITE;
+
+	bResult &= VirtualProtect(lpAddress, dwCount, dwNewProtect, &dwOldProtect);		// Allow read/write access
+	bResult &= WriteProcessMemory(hProcess, lpAddress, lpBuffer, dwCount, NULL);	// Write to process memory
+	bResult &= VirtualProtect(lpAddress, dwCount, dwOldProtect, &dwNewProtect);		// Restore original access
+	bResult &= FlushInstructionCache(hProcess, lpAddress, dwCount);					// Update instruction cache
+
+	return bResult;
+}
+
+// Finds all locations of a signature
+std::vector<DWORD> FindSignature(LPBYTE lpBuffer, DWORD dwBufferSize, LPBYTE lpSignature, DWORD dwSignatureSize, LPBYTE lpWildCards = 0)
+{
+	// List of locations of the signature
+	std::vector<DWORD> addresses;
+
+	// Loop through each byte in the buffer
+	for (DWORD i = 0; i < dwBufferSize; ++i)
+	{
+		bool bFound = true;
+
+		// Loop through each byte in the signature
+		for (DWORD j = 0; j < dwSignatureSize; ++j)
+		{
+			// Check if the index overruns the buffer
+			if (i + j >= dwBufferSize)
+			{
+				bFound = false;
+
+				break;
+			}
+
+			// Check if wild cards are used
+			if (lpWildCards)
+			{
+				// Check if the buffer does not equal the signature and a wild card is not set
+				if (lpBuffer[i + j] != lpSignature[j] && !lpWildCards[j])
+				{
+					bFound = false;
+
+					break;
+				}
+			}
+			else
+			{
+				// Check if the buffer does not equal the signature
+				if (lpBuffer[i + j] != lpSignature[j])
+				{
+					bFound = false;
+
+					break;
+				}
+			}
+		}
+
+		// Check if the signature was found and add it to the address list
+		if (bFound)
+			addresses.push_back(i);
+	}
+
+	// Return the list of locations of the signature
+	return addresses;
+}
+
+// Finds the location of a signature
+DWORD FindAddress(LPBYTE lpBuffer, DWORD dwBufferSize, LPBYTE lpSignature, DWORD dwSignatureSize, LPBYTE lpWildCards = 0, DWORD dwIndex = 0, DWORD dwOffset = 0)
+{
+	DWORD dwAddress = 0;
+
+	// Find all locations of the signature
+	std::vector<DWORD> addresses = FindSignature(lpBuffer, dwBufferSize, lpSignature, dwSignatureSize, lpWildCards);
+
+	// Return the requested address
+	if (addresses.size() - 1 >= dwIndex)
+		dwAddress = (DWORD)lpBuffer + addresses[dwIndex] + dwOffset;
+
+	return dwAddress;
+}
+
+//
+//-------------------------------------------------------------------------------------------------
+// Halo-specific code
+//
+
+char* histBuff;
+UINT16* histBuffLen;
+
+/* Ensures that your custom command is added to the console's history (up/down arrows)
+ * should the user want to use it again without retyping.
+ */
+void bufferCommand(char* input) {
+    // Command buffer is a fixed array, 8*255 bytes
+    *histBuffLen = (*histBuffLen + 1) % 8;
+    char *location = (*histBuffLen * 255) + histBuff;
+    strcpy_s(location, 255, input);
+}
+
+/* All console commands entered by the player flow through this function.
+ * If the command matches one of HAC's, handle it and alter the command
+ * buffer to prevent Halo from displaying a 'command not found' error.
+ */
+void ConsoleRead(char* input) {
+    // set to true if handled a command, otherwise leave false to pass through to Halo
+    bool processed = false;
+
+    // TODO Handle commands here
+
+    if(processed) {
+        bufferCommand(input);
+        input[0] = 0x3B; // prevent hooked function from processing
+    }
+}
+
+//
+//-------------------------------------------------------------------------------------------------
+// Code Caves
+//
+
+void _declspec(naked) ConsoleReadCC() {
+    __asm {
+
+        pushfd
+        pushad
+        push edi
+
+        call ConsoleRead
+
+        add esp, 4
+        popad
+        popfd
+
+        //Original
+        mov al, [edi]
+        sub esp, 0x00000500
+        jmp ProcessCommands // Where should this go?
+    }
+}
+
+// Creates a code cave to a function at a specific address
+BOOL CreateCodeCave(DWORD dwAddress, BYTE cbSize, VOID (*pFunction)())
+{
+	BOOL bResult = TRUE;
+
+	if (cbSize < 5)
+		return FALSE;
+
+	// Calculate the offset from the function to the address
+	DWORD dwOffset = PtrToUlong(pFunction) - dwAddress - 5;
+
+	// Construct the call instruction to the offset
+	BYTE patch[0xFF] = {0x90};
+	patch[0] = 0xE8;
+	memcpy(patch + 1, &dwOffset, sizeof(dwAddress));
+
+	// Write the code cave to the address
+	bResult &= WriteBytes(dwAddress, patch, cbSize);
+
+	return bResult;
+}
+
+//
+//-------------------------------------------------------------------------------------------------
+//
+
 DllExport void InjectedMain(const wchar_t * argument)
 {
-    DisplayPid();
+    // Get address of code section
+	LPBYTE lpModule = (LPBYTE)GetModuleHandle(0);
+	DWORD dwOffsetToPE = *(DWORD*)(lpModule + 0x3C);
+	DWORD dwSizeOfCode = *(DWORD*)(lpModule + dwOffsetToPE + 0x1C);
+	DWORD dwBaseOfCode = *(DWORD*)(lpModule + dwOffsetToPE + 0x2C);
+	LPBYTE lpCode = (LPBYTE)(lpModule + dwBaseOfCode);
+
+    // Find the address of histBuff
+	BYTE histBuffSig[] = {0x8D, 0x64, 0x24, 0x00, 0x8A, 0x0A, 0x42, 0x88, 0x08, 0x40, 0x3A, 0xCB, 0x75};
+	DWORD pHistBuff = FindAddress(lpCode, dwSizeOfCode, histBuffSig, sizeof(histBuffSig)) - 0x5;
+    histBuff = (char*)*(UINT32*)pHistBuff;
+
+    // Find the address of histBuffLen
+    BYTE histBuffLenSig[] = {0x0F, 0xBF, 0xC0, 0x69, 0xC0, 0xFF, 0x00, 0x00, 0x00, 0x8B, 0xD7, 0x05};
+	DWORD pHistBuffLen = FindAddress(lpCode, dwSizeOfCode, histBuffSig, sizeof(histBuffLenSig)) - 0x5;
+    histBuffLen = (UINT16*)*(UINT32*)pHistBuffLen;
+
+    // Find the address of the histBuff and histBuffLen
+	BYTE sig[] = {0x8D, 0x64, 0x24, 0x00, 0x8A, 0x0A, 0x42, 0x88, 0x08, 0x40, 0x3A, 0xCB, 0x75};
+	DWORD cc_onload = FindAddress(lpCode, dwSizeOfCode, sig, sizeof(sig));
+
+    DWORD cc_consoleread = 0x4C6C40;
+
+	// Check if the signature was found and create the code cave
+	if (cc_onload)
+        CreateCodeCave(cc_consoleread, 5, ConsoleReadCC);
 }
